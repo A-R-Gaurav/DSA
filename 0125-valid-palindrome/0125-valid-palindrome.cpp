66dfea0b28ae80d3cd491555dbac853026c3af15
Ankit Raj Gaurav <aargauravsingh@gmail.com>
2026-09-16 15:40:05 +0530

    Time: 1 ms (42.77%), Space: 9.8 MB (77%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int st = 0;
+        int end = s.size() - 1;
+
+        while (st < end) {
+
+            // Skip non-alphanumeric characters
+            while (st < end && !isalnum(s[st])) {
+                st++;
+            }
+
+            while (st < end && !isalnum(s[end])) {
+                end--;
+            }
+
+            // Compare after converting to lowercase
+            if (tolower(s[st]) != tolower(s[end])) {
+                return false;
+            }
+
+            st++;
+            end--;
+        }
+
+        return true;
+    }
+};
